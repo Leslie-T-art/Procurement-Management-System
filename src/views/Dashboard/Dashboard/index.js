86a887831fc2d9b5242c3e15fd3ea9ccd5ad8d31
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-09-18 10:36:29 +0200

    Update index.js

--- src/views/Dashboard/Dashboard/index.js
+++ src/views/Dashboard/Dashboard/index.js
@@ -66,7 +66,7 @@ export default function Dashboard() {
         gap='24px'>
         <BuiltByDevelopers
           title={"Built by Developers"}
-          name={"Purity UI Dashboard"}
+          name={"PROCUREMENT DASHBOARD"}
           description={
             "From colors, cards, typography to complex elements, you will find the full documentation."
           }
